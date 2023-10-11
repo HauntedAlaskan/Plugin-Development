10482ac8e68645692da4ee71ca97903f2f64b681
HauntedAlaskan <132791406+HauntedAlaskan@users.noreply.github.com>
2023-10-11 09:51:45 -0400

    Update test.lua

--- rbx/test.lua
+++ rbx/test.lua
@@ -1,1 +1,25 @@
+local plr = game.Players.LocalPlayer
+local rs = game:GetService("ReplicatedStorage")
+local servstor = game:GetService("ServerStorage")
 
+local rstools = rs:WaitForChild("Tools")
+local servtools = servstor:WaitForChild("Tools")
+
+local function give()
+  for i,v in pairs(rstools) do
+    if v:IsA("Tool") then
+      local clone = v:Clone()
+      clone.Parent = plr.Backpack
+    end
+  end
+  for i,v in pairs(servtools) do
+    if v:IsA("Tool") then
+      local clone = v:Clone()
+      clone.Parent = plr.Backpack
+    end
+  end
+end
+
+while plr.Backpack:GetChildren() == nil or " " do
+  give()
+end
